9c3808f007229c96c3b7c58a168b34e4c0585f18
Tokyo <moudi0u0@gmail.com>
2024-10-29 18:48:47 +0200

    Encapsulation And Abstraction

--- Form1.cs
+++ Form1.cs
@@ -5,7 +5,102 @@ using System.Windows.Forms;
 
 
 
+class clsCalculator 
+{
+    double _Num1;
+    double _Num2;
+    string _ShowNumClick;
+    char _Opr;
 
+
+    public double Num1
+    {
+        get { return _Num1; }
+        set { _Num1 = value; }
+    }
+
+    public double Num2
+    {
+        get { return _Num2; }
+        set { _Num2 = value; }
+    }
+
+    public string ShowNumClick
+    {
+        get { return _ShowNumClick; }
+        set { _ShowNumClick = value; }
+    }
+
+    public char Opr
+    {
+        get { return _Opr; }
+        set { _Opr = value; }
+    }
+
+    public void Clear(TextBox txt)
+    {
+        _Num1 = 0;
+        _Num2 = 0;
+        txt.Text = "";
+        _ShowNumClick = "";
+    }
+
+    public double Claculator(double Num1, double Num2)
+    {
+        switch (_Opr)
+        {
+            case '+':
+                return Num1 + Num2;
+
+            case '-':
+                return Num1 - Num2;
+
+            case '/':
+                return Num1 / Num2;
+
+            case '*':
+                return Num1 * Num2;
+
+            case '%':
+                return Num1 * Num2;
+
+            default:
+                return 0;
+        }
+    }
+
+    public void ReadBtn(Button btn, TextBox txt)
+    {
+        _ShowNumClick += btn.Tag.ToString();
+        txt.Text = _ShowNumClick;
+    }
+
+    public void ReadBtnOpr(Button btn, TextBox txt)
+    {
+        string Str = btn.Tag.ToString();
+        _Opr = Str[0];
+
+        txt.Text = _Opr.ToString();
+
+        if (!(_ShowNumClick == ""))
+        {
+            _Num1 = Convert.ToSingle(_ShowNumClick);
+        }
+        _ShowNumClick = "";
+    }
+
+    public void Equale(TextBox txt)
+    {
+        if (!(_ShowNumClick == ""))
+        {
+            _Num2 = Convert.ToSingle(_ShowNumClick);
+        }
+
+        _ShowNumClick = "";
+        txt.Text = Claculator(_Num1, _Num2).ToString();
+        _Num1 = Claculator(_Num1, _Num2);
+    }
+}
 
 
 
@@ -16,98 +111,31 @@ namespace Calculator_1
     public partial class Form1 : Form
     {
 
-        string ShowNumClick = "";
-        char Opr;
-        double Number1;
-        double Number2;
-
-
-
-        public double Claculator(double Num1, double Num2)
-        {
-            switch (Opr)
-            {
-                case '+':
-                    return Num1 + Num2;
-
-                case '-':
-                    return Num1 - Num2;
-
-                case '/':
-                    return Num1 / Num2;
-
-                case '*':
-                    return Num1 * Num2;
-
-                default:
-                    return 0;
-            }
-        }
+        clsCalculator Calc = new clsCalculator();
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        public void ReadBtn(Button btn)
-        {
-            ShowNumClick += btn.Tag.ToString();
-            txtTotal.Text = ShowNumClick;
-        }
-
-        public void ReadBtnOpr(Button btn)
-        {
-            string Str = btn.Tag.ToString();
-            Opr = Str[0];
-
-            txtTotal.Text = Opr.ToString();
-
-            if (!(ShowNumClick == ""))
-            {
-                Number1 = Convert.ToSingle(ShowNumClick);
-            }
-            ShowNumClick = "";
-        }
-
-        public void Reset()
-        {
-            txtTotal.Text = "";
-            Number1 = 0;
-            Number2 = 0;
-            ShowNumClick = "";
-        }
-
-        public void Equale()
-        {
-            if (!(ShowNumClick == ""))
-            {
-                Number2 = Convert.ToSingle(ShowNumClick);
-            }
-            ShowNumClick = "";
-            txtTotal.Text = Claculator(Number1, Number2).ToString();
-            Number1 = Claculator(Number1, Number2);
-            Number2 = 0;
-        }
-    
-
         private void btn_Click(object sender, EventArgs e)
         {
-            ReadBtn((Button)sender);
+            Calc.ReadBtn((Button)sender, txtTotal);
         }
 
         private void btnOpr_Click(object sender, EventArgs e)
         {
-            ReadBtnOpr((Button)sender);
+            Calc.ReadBtnOpr((Button)sender, txtTotal);
         }
 
         private void btnClear_Click(object sender, EventArgs e)
         {
-            Reset();
+            Calc.Clear(txtTotal);
         }
 
         private void btnEqual_Click(object sender, EventArgs e)
         {
-            Equale();
+            Calc.Equale(txtTotal);
         }
     }
 }
@@ -116,28 +144,3 @@ namespace Calculator_1
 
 
 
-//class clsCalculator : Form1
-//{
-//    double _Num1;
-//    double _Num2;
-
-
-//    public double Num1
-//    {
-//        get { return _Num1; }
-//        set { _Num1 = value; }
-//    }
-
-//    public double Num2
-//    {
-//        get { return _Num2; }
-//        set { _Num2 = value; }
-//    }
-
-
-//    public void Clear()
-//    {
-//        _Num1 = 0;
-//        _Num2 = 0;
-//    }
-//}
